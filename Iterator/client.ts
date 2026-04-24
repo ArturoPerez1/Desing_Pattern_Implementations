3fcdaaa6f5d55940c56c07bfd690d4e7d35d362f
ArturoPerez <arjperez31@gmail.com>
2026-04-24 19:41:59 -0400

    adding Iterator Pattern Implementation

--- Iterator/client.ts
+++ Iterator/client.ts
@@ -2,9 +2,9 @@ import { AggregateArray } from "./aggregate";
 
 function main(){
     let aggregateArray = new AggregateArray<string>;
-    aggregateArray.addItem("Arturo");
-    aggregateArray.addItem("Arturo");
-    aggregateArray.addItem("Arturo");
+    aggregateArray.addItem("Item1");
+    aggregateArray.addItem("Item2");
+    aggregateArray.addItem("Item3");
     let iterator = aggregateArray.getIterator();
 
     console.log(iterator.next());
