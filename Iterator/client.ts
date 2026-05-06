d60927ae3929cf53d02e99e8461cb537928ac211
ArturoPerez <arjperez31@gmail.com>
2026-05-05 21:56:49 -0400

    fixing bug in Iterator Pattern Implementation

--- Iterator/client.ts
+++ Iterator/client.ts
@@ -1,15 +1,13 @@
 import { AggregateArray } from "./aggregate.ts";
 
-function main(): void{
-    let aggregateArray = new AggregateArray<string>;
-    aggregateArray.addItem("Item1");
-    aggregateArray.addItem("Item2");
-    aggregateArray.addItem("Item3");
-    let iterator = aggregateArray.getIterator();
+let aggregateArray = new AggregateArray<string>;
+aggregateArray.addItem("Item1");
+aggregateArray.addItem("Item2");
+aggregateArray.addItem("Item3");
+let iterator = aggregateArray.getIterator();
 
-    console.log(iterator.next());
-    console.log(iterator.hasNext());
-    console.log(iterator.next());
-}
+console.log(iterator.next());
+console.log(iterator.hasNext());
+console.log(iterator.next());
 
-main();
+
